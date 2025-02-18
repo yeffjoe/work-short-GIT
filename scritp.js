419ff89311be3da404aa94643b856594a43c7c5d
yeffry ramirez <yeffry535@gmail.com>
2025-02-18 11:03:21 -0500

    se agrego javascript

---
+++ scritp.js
@@ -0,0 +1,16 @@
+alert ( "probando script de manera externa")
+let nombre = "yeffry ramirez"
+let edad = 19
+let ciudad " Garzon-huila";
+console.long (" mi nombre es"+ "nombre", "tengo" + edad + "años y vivo en" + ciudad)
+
+
+
+/* crea una variable llamada precio y asignale un valor decimal luego crea otra variable llamada cantidad
+ en un valor entero. muestra el tipo de datos de cada variable.*/
+ let precio = 3.14
+ let cantidad = 5;
+ console.log(typeof precio);//imprime en la consola 
+ console.long(typeof cantidad);//imprime en la carpeta
+ documento.wri
+ alert(typeof cantidad )
